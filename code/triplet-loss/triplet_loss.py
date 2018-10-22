e7913b9bbd8c09b93f681d5cd86de9fbace313e6
lawlite <849451478@qq.com>
2018-10-22 17:07:53 +0800

    add codes for triplet loss

--- code/triplet-loss/triplet_loss.py
+++ code/triplet-loss/triplet_loss.py
@@ -44,36 +44,121 @@ def _get_triplet_mask(labels):
           mask: 3D,shape = (batch_size, batch_size, batch_size)
     
     '''
+    
+    # 初始化一个二维矩阵，坐标(i, j)不相等置为1，得到indices_not_equal
     indices_equal = tf.cast(tf.eye(tf.shape(labels)[0]), tf.bool)
     indices_not_equal = tf.logical_not(indices_equal)
-    i_not_equal_j = tf.expand_dims(indices_not_equal, 2)
+    # 因为最后得到一个3D的mask矩阵(i, j, k)，增加一个维度，则 i_not_equal_j 在第三个维度增加一个即，(batch_size, batch_size, 1), 其他同理
+    i_not_equal_j = tf.expand_dims(indices_not_equal, 2) 
     i_not_equal_k = tf.expand_dims(indices_not_equal, 1)
     j_not_equal_k = tf.expand_dims(indices_not_equal, 0)
-
+    # 想得到i!=j!=k, 三个不等取and即可, 最后可以得到当下标（i, j, k）不相等时才取True
     distinct_indices = tf.logical_and(tf.logical_and(i_not_equal_j, i_not_equal_k), j_not_equal_k)
 
+    # 同样根据labels得到对应i=j, i!=k
     label_equal = tf.equal(tf.expand_dims(labels, 0), tf.expand_dims(labels, 1))
     i_equal_j = tf.expand_dims(label_equal, 2)
     i_equal_k = tf.expand_dims(label_equal, 1)
     valid_labels = tf.logical_and(i_equal_j, tf.logical_not(i_equal_k))
+    # mask即为满足上面两个约束，所以两个3D取and
     mask = tf.logical_and(distinct_indices, valid_labels)
     return mask
 
 def batch_all_triplet_loss(labels, embeddings, margin, squared=False):
+    '''
+       triplet loss of a batch
+       -------------------------------
+       Args:
+          labels:     标签数据，shape = （batch_size,）
+          embeddings: 提取的特征向量， shape = (batch_size, vector_size)
+          margin:     margin大小， scalar
+          
+       Returns:
+          triplet_loss: scalar, 一个batch的损失值
+          fraction_postive_triplets : valid的triplets占的比例
+    '''
+    
+    # 得到每两两embeddings的距离，然后增加一个维度，一维需要得到（batch_size, batch_size, batch_size）大小的3D矩阵
+    # 然后再点乘上valid 的 mask即可
     pairwise_dis = _pairwise_distance(embeddings, squared=squared)
-    anchor_positive_dist = tf.exoand_dims(pairwise_dis, 2)
+    anchor_positive_dist = tf.expand_dims(pairwise_dis, 2)
     assert anchor_positive_dist.shape[2] == 1, "{}".format(anchor_positive_dist.shape)
     anchor_negative_dist = tf.expand_dims(pairwise_dis, 1)
     assert anchor_negative_dist.shape[1] == 1, "{}".format(anchor_negative_dist.shape)
-
     triplet_loss = anchor_positive_dist - anchor_negative_dist + margin
+    
     mask = _get_triplet_mask(labels)
     mask = tf.to_float(mask)
     triplet_loss = tf.multiply(mask, triplet_loss)
     triplet_loss = tf.maximum(triplet_loss, 0.0)
+    
+    # 计算valid的triplet的个数，然后对所有的triplet loss求平均
     valid_triplets = tf.to_float(tf.greater(triplet_loss, 1e-16))
     num_positive_triplets = tf.reduce_sum(valid_triplets)
     num_valid_triplets = tf.reduce_sum(mask)
     fraction_postive_triplets = num_positive_triplets / (num_valid_triplets + 1e-16)
+    
     triplet_loss = tf.reduce_sum(triplet_loss) / (num_positive_triplets + 1e-16)
     return triplet_loss, fraction_postive_triplets
+
+def _get_anchor_positive_triplet_mask(labels):
+    ''' 
+       得到合法的positive的mask， 即2D的矩阵，[a, p], a!=p and a和p相同labels
+       ------------------------------------------------
+       Args:
+          labels: 标签数据，shape = (batch_size, )
+          
+       Returns:
+          mask: 合法的positive mask, shape = (batch_size, batch_size)
+    '''
+    indices_equal = tf.cast(tf.eye(tf.shape(labels)[0]), tf.bool)
+    indices_not_equal = tf.logical_not(indices_equal)                 # （i, j）不相等
+    labels_equal = tf.equal(tf.expand_dims(labels, 0), tf.expand_dims(labels, 1))  # labels相等，
+    mask = tf.logical_and(indices_not_equal, labels_equal)            # 取and即可
+    return mask
+
+def _get_anchor_negative_triplet_mask(labels):
+    '''
+       得到negative的2D mask, [a, n] 只需a, n不同且有不同的labels
+       ------------------------------------------------
+       Args:
+          labels: 标签数据，shape = (batch_size, )
+          
+       Returns:
+          mask: negative mask, shape = (batch_size, batch_size)
+    '''
+    labels_equal = tf.equal(tf.expand_dims(labels, 0), tf.expand_dims(labels, 1))
+    mask = tf.logical_not(labels_equal)
+    return mask
+
+def batch_hard_triplet_loss(labels, embeddings, margin, squared=False):
+    '''
+       batch hard triplet loss of a batch， 每个样本最大的positive距离 - 对应样本最小的negative距离
+       ------------------------------------
+       Args:
+          labels:     标签数据，shape = （batch_size,）
+          embeddings: 提取的特征向量， shape = (batch_size, vector_size)
+          margin:     margin大小， scalar
+          
+       Returns:
+          triplet_loss: scalar, 一个batch的损失值
+    '''
+    pairwise_distances = _pairwise_distance(embeddings)
+    mask_anchor_positive = _get_anchor_positive_triplet_mask(labels)
+    mask_anchor_positive = tf.to_float(mask_anchor_positive)
+    anchor_positive_dist = tf.multiply(mask_anchor_positive, pairwise_distances)
+    hardest_positive_dist = tf.reduce_max(anchor_positive_dist, axis=1, keepdims=True)  # 取每一行最大的值即为最大positive距离
+    tf.summary.scalar("hardest_positive_dis", tf.reduce_mean(hardest_positive_dist))
+    
+    '''取每一行最小值得时候，因为invalid [a, n]置为了0， 所以不能直接取，这里对应invalid位置加上每一行的最大值即可，然后再取最小的值'''
+    mask_anchor_negative = _get_anchor_negative_triplet_mask(labels)
+    mask_anchor_negative = tf.to_float(mask_anchor_negative)
+    max_anchor_negative_dist = tf.reduce_max(pairwise_distances, axis=1, keepdims=True)   # 每一样最大值
+    anchor_negative_dist = pairwise_distances + max_anchor_negative_dist * (1.0 - mask_anchor_negative)  # (1.0 - mask_anchor_negative)即为invalid位置
+    hardest_negative_dist = tf.reduce_min(anchor_negative_dist, axis=1, keepdims=True)
+    tf.summary.scalar("hardest_negative_dist", tf.reduce_mean(hardest_negative_dist))
+    
+    triplet_loss = tf.maximum(hardest_positive_dist - hardest_negative_dist + margin, 0.0)
+    triplet_loss = tf.reduce_mean(triplet_loss)
+    return triplet_loss
+    
--- code/triplet-loss/triplet_loss_np.py
+++ code/triplet-loss/triplet_loss_np.py
@@ -27,38 +27,46 @@ def test_pairwise_distances(squared = False):
 def test_get_triplet_mask(labels):
     '''
     valid （i, j, k）满足
-         - i, j, k都不相等
+         - i, j, k 不相等
          - labels[i] == labels[j]  && labels[i] != labels[k]
     
-    array([[[False, False, False],
-            [False, False, False],
-            [False,  True, False]],
-
-          [[False, False, False],
-           [False, False, False],
-           [False, False, False]],
-
-          [[False,  True, False],
-           [False, False, False],
-           [False, False, False]]])
     '''
+    # 初始化一个二维矩阵，坐标(i, j)不相等置为1，得到indices_not_equal    
     indices_equal = np.cast[np.bool](np.eye(np.shape(labels)[0], dtype=np.int32))
     indices_not_equal = np.logical_not(indices_equal)
+    # 因为最后得到一个3D的mask矩阵(i, j, k)，增加一个维度，则 i_not_equal_j 在第三个维度增加一个即，(batch_size, batch_size, 1), 其他同理    
     i_not_equal_j = np.expand_dims(indices_not_equal, 2)
     i_not_equal_k = np.expand_dims(indices_not_equal, 1)
     j_not_equal_k = np.expand_dims(indices_not_equal, 0)
-
+    # 想得到i!=j!=k, 三个不等取and即可
+    # 比如这里得到
+    '''array([[[False, False, False],
+               [False, False,  True],
+               [False,  True, False]],
+              [[False, False,  True],
+               [False, False, False],
+               [ True, False, False]],
+              [[False,  True, False],
+              [ True, False, False],
+              [False, False, False]]])'''
+    # 只有下标(i, j, k)不相等时才是True
     distinct_indices = np.logical_and(np.logical_and(i_not_equal_j, i_not_equal_k), j_not_equal_k)
+    
+    # 同样根据labels得到对应i=j, i!=k
     label_equal = np.equal(np.expand_dims(labels, 0), np.expand_dims(labels, 1))
     i_equal_j = np.expand_dims(label_equal, 2)
     i_equal_k = np.expand_dims(label_equal, 1)
-    
     valid_labels = np.logical_and(i_equal_j, np.logical_not(i_equal_k))
+    
+    # mask即为满足上面两个约束，所以两个3D取and
     mask = np.logical_and(valid_labels, distinct_indices)
     return mask
     
 
 def test_batch_all_triplet_loss(margin):
+    
+    # 得到每两两embeddings的距离，然后增加一个维度，一维需要得到（batch_size, batch_size, batch_size）大小的3D矩阵
+    # 然后再点乘上valid 的 mask即可    
     labels = np.array([1, 0, 1])   # 比如1，3是正例，2是负例，这样计算出的loss应该是16-8 = 8
     pairwise_distances = test_pairwise_distances()
     anchor_positive = np.expand_dims(pairwise_distances, axis=2)
@@ -77,9 +85,43 @@ def test_batch_all_triplet_loss(margin):
     
     triplet_loss = np.sum(triplet_loss) / (num_positive_triplet + 1e-16)
     return triplet_loss, fraction_positive_triplet
+
+def test_anchor_positive_triplet_mask(labels):  
+    # 得到positive的2D mask， i!=j and i和j有相同labels
+    indices_equal = np.cast[np.bool](np.eye(np.shape(labels)[0]))
+    indices_not_equal = np.logical_not(indices_equal)
+    labels_equal = np.equal(np.expand_dims(labels, 0), np.expand_dims(labels, 1))
+    mask = np.logical_and(indices_not_equal, labels_equal)
+    return mask
     
+def test_get_anchor_negative_triplet_mask(labels):
+    # 得到negative的2D mask
+    labels_equal = np.equal(np.expand_dims(labels, 0), np.expand_dims(labels, 1))
+    mask = np.logical_not(labels_equal)
+    return mask
     
+def test_batch_hard_triplet_loss(margin):
+    # 还是得到两两的距离pairwise_distances
+    # 计算最大的positive距离，只需要取每行最大元素即可
+    # 计算最小的negative距离，不能直接取每行最小的元素，
+    labels = np.array([1, 0, 1])
+    pairwise_distances = test_pairwise_distances()
+    mask_anchor_positive = test_anchor_positive_triplet_mask(labels)
+    mask_anchor_positive = np.cast[np.float](mask_anchor_positive)
+    anchor_positive_dist = np.multiply(mask_anchor_positive, pairwise_distances)
+    hardest_positive_dist = np.max(anchor_positive_dist, axis=1, keepdims=True)
+    
+    mask_anchor_negative = test_get_anchor_negative_triplet_mask(labels)
+    mask_anchor_negative = np.cast[np.float](mask_anchor_negative)
+    
+    max_anchor_negative_dist = np.max(pairwise_distances, axis=1, keepdims=True)
+    anchor_negative_dist = pairwise_distances + max_anchor_negative_dist * (1.0 - mask_anchor_negative)
+    hardest_negative_dist = np.min(anchor_negative_dist, axis=1, keepdims=True)
+    triplet_loss = np.maximum(hardest_positive_dist - hardest_negative_dist + margin, 0.0)
+    triplet_loss = np.mean(triplet_loss)
+    
+    return triplet_loss
 
 if __name__ == '__main__':
-    # test_pairwise_distances()
-    test_batch_all_triplet_loss(margin = 0.0)
+    #test_batch_all_triplet_loss(margin = 0.0)
+    test_batch_hard_triplet_loss(margin = 0.0)
